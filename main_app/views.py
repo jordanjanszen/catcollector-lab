00bce122e7f88cb90f4bf889dcf59d9f13c0d7e1
Jim Clark <jim.clark@generalassemb.ly>
2022-11-01 15:24:04 -0700

    sync-22-finish-many

--- main_app/views.py
+++ main_app/views.py
@@ -79,3 +79,7 @@ class ToyDelete(DeleteView):
 def assoc_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.add(toy_id)
   return redirect('detail', cat_id=cat_id)
+
+def unassoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
